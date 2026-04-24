75c1ebb370b47a8430747a28fb6e3323cbde9e25
carlosao24 <carloslopez21trabajos@gmail.com>
2026-04-23 22:12:20 -0500

    Mejoras visuales con ayuda de la IA

--- tablas/multiplicación.js
+++ tablas/multiplicación.js
@@ -1,10 +1,25 @@
 function generarTabla(){
     let contenido = ""
     let contenedor = document.getElementById("contenedorTabla")
+    let valor = document.getElementById("txtCaja1")
+    let n = valor.value;
+    let numeroTabla = parseInt(n)
     for(let i =1; i < 11; i++){
-        contenido = contenido + "<div class='table-row' ><span>5 x " + (i) + "=</span> <strong>"+ (5 * i) + "</strong></div>"
+        contenido = contenido + "<div class='table-row' ><span>"+ numeroTabla +"x" + (i) + "=</span> <strong>"+ (numeroTabla * i) + "</strong></div>"
         console.log(contenido)
-        contenedor.innerHTML = contenido
+        contenedor.innerHTML = contenido 
     }
-    
+    mostrarTexto(numeroTabla);
+}
+
+function mostrarTexto(valor){
+    let numero = document.getElementById("numero")
+    let mostrarNumero = ""
+    mostrarNumero = mostrarNumero + "<h2 id='numero'><span>TABLA DEL "+valor+"</span></h2>"
+    console.log(mostrarNumero)
+    numero.innerHTML = mostrarNumero
+}
+
+function reiniciar(){
+    location.reload()
 }
